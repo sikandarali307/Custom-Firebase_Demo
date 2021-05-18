41f6caae4389186dec845e49eeeee8e182388778
sikandar <sikandar.ali307@gmail.com>
2021-05-18 11:31:14 +0500

    Initial Commit

---
+++ Custom-Firebase_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom-Firebase_Demo
+//
+//  Created by Sikandar Ali on 18/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
